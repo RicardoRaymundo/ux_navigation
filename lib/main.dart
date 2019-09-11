250a1cd9b0b50b9762b338889ce598689325f184
macbookpro <1>
2019-09-11 15:57:07 -0300

    - Criada a pagina de login e main page - Criados os componentes de paginas de home page, search page, gallery page downloads page e more page - implementada uma custom bottombar para a navegação entre as paginas da main page

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:ux_navigation/app/app_behavior.dart';
+import 'package:ux_navigation/app/app_fonts.dart';
+import 'package:ux_navigation/pages/pages_login.dart';
+import 'package:ux_navigation/pages/pages_main.dart';
+
+void main() {
+  //Orientação do app
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+  runApp(MainApp());
+}
+
+class MainApp extends StatefulWidget {
+
+  @override
+  _MainAppState createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+
+  final bool isLogged = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'UX Navigation',
+      builder: (context, child) {
+        return ScrollConfiguration(
+          behavior: AppBehavior(),
+          child: child,
+        );
+      },
+      theme: ThemeData(
+        backgroundColor: Colors.transparent,
+        fontFamily: AppFonts.quickFont,
+        brightness: Brightness.dark,
+        primaryColor: Color(0xff2e2e2e),
+        accentColor: Color(0xff2e2e2e),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: home(),
+      routes: <String, WidgetBuilder>{
+        /// Rotas do app
+        '/pagesMain': (BuildContext context) => PagesMain(),
+        '/pagesLogin': (BuildContext context) => PagesLogin(),
+      },
+    );
+  }
+
+  Widget home() {
+    /// Se estiver logado, carrega a MainPage
+    /// Se nao estiver logado, carrega a LoginPage
+    return isLogged ? PagesMain() : PagesLogin();
+  }
+}
